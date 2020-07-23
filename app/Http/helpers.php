9ae723f6a443dfbb95e8669025affa732de8bf55
Sortia <alexkiyan.lug@gmail.com>
2020-07-23 12:07:26 +0300

    fix print empty task

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Blade;
 
-function print_task(string $task) {
+function print_task(?string $task) {
     $php = Blade::compileString($task);
     $obLevel = ob_get_level();
     ob_start();
