86cb83896abbefbf8efed61e74f66133a3caa25f
Sortia <alexkiyan.lug@gmail.com>
2020-07-25 14:44:27 +0300

    опции для редактора

--- public/js/editor.js
+++ public/js/editor.js
@@ -5,6 +5,7 @@ $(() => {
             toolbar: [
                 ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
                 ['blockquote', 'code-block'],
+                ['link', 'image', 'video'],
 
                 [{ 'header': 1 }, { 'header': 2 }],               // custom button values
                 [{ 'list': 'ordered'}, { 'list': 'bullet' }],
@@ -16,7 +17,6 @@ $(() => {
                 [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 
                 [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-                [{ 'font': [] }],
                 [{ 'align': [] }],
 
                 ['clean']                                         // remove formatting button
