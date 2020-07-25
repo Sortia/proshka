dda29d65adf816363921f2ed50621e1f9a287559
Sortia <alexkiyan.lug@gmail.com>
2020-07-25 14:43:12 +0300

    опции для редактора

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,7 +2,25 @@ $(() => {
     window.editor = new Quill('#editor', {
         theme: 'snow',
         modules: {
-            toolbar: [['bold', 'italic'], ['link', 'image', 'video']],
+            toolbar: [
+                ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+                ['blockquote', 'code-block'],
+
+                [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+                [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+                [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+                [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+                [{ 'direction': 'rtl' }],                         // text direction
+
+                [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+                [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+                [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+                [{ 'font': [] }],
+                [{ 'align': [] }],
+
+                ['clean']                                         // remove formatting button
+            ],
         }
     });
 });
