b6fdaf2079fc45d8a073cb3ba1246b753d93696c
Alex <alexkiyan.lug@gmail.com>
2020-11-11 16:16:30 +0300

    seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(UserSeeder::class);
         $this->call(RoleSeeder::class);
+        $this->call(LessonStatusSeeder::class);
 
         Schema::enableForeignKeyConstraints();
     }
